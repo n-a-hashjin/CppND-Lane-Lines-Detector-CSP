3022d49df60b27e2e5f8a707425b1166e7d13575
nasser-azarbakhsh-hashjin <azarbakhsh.n@gmail.com>
2020-11-25 23:45:09 +0100

    feat: Add masking functionality
    Add new image processing feature to program in order to lane line detection using masking technique in order to masking unwanted areas out.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
-#include <iostream>
-#include <string>
 #include "file_handle.h"
 #include "camera_calibration.h"
 #include "preprocessing.h"
-#include <unistd.h>
+#include "image_processing.h"
+
 #include <opencv2/opencv.hpp>
+#include <unistd.h>
+#include <iostream>
+#include <string>
+#include <vector>
 
 
 enum Param {
@@ -23,8 +26,29 @@ int main() {
     
     cv::Mat ppFrame;
     cv::Mat frame = cv::imread("../imgs/test2.jpg");
+
     preprocessing preproc;
     preproc.runPreprocessing(frame, ppFrame);
+    
+    vector<vector<int>> vertices;
+    vector<int> top_left, bottom_left, bottom_right, top_right;
+    bottom_left.assign({0, 670});
+    bottom_right.assign({1280, 670});
+    top_right.assign({700, 430});
+    top_left.assign({572, 430});
+
+    vertices.push_back(bottom_left);
+    vertices.push_back(bottom_right);
+    vertices.push_back(top_right);
+    vertices.push_back(top_left);
+    
+    cv::Mat result;
+    ImageProcessing imgProc(vertices);
+    imgProc.regionOfInterest(ppFrame, result);
+    std::cout << "size of original frame: " << ppFrame.size() << std::endl;
+    std::cout << "size of original result: " << result.size() << std::endl;
+    cv::imshow("result", result);
+    cv::waitKey();
     /*
     std::string path = "../imgs/";
     unsigned int chess_size[2] = {9,6};
